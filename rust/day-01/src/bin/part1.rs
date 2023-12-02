923a783b481a5b2355db5c52a8bf2175d4b6d80d
James Rouzier <jrouzier@inverse.ca>
2023-12-02 20:43:15 +0000

    Move inputs to a common folder

--- rust/day-01/src/bin/part1.rs
+++ rust/day-01/src/bin/part1.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let input = include_str!("input1.txt");
+    let input = include_str!("../../../../inputs/day-01/input1.txt");
     let mut a = 0;
     for l in input.split('\n') {
         if l.len() == 0 {
