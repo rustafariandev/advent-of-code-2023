698592748a77a28df3aa386ac982ddfda2b4cb4f
James Rouzier <jrouzier@inverse.ca>
2023-12-03 20:34:55 +0000

    remove main.rs

--- new_aoc.sh
+++ new_aoc.sh
@@ -3,5 +3,6 @@
 cargo new --vcs=none rust/$1
 mkdir -p rust/$1/src/bin perl/$1 inputs/$1
 cp part.rs rust/$1/src/bin/part1.rs
+rm rust/$1/src/main.rs
 cp part.pl perl/$1/part1.pl
 touch inputs/$1/inputs1.txt
